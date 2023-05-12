e72996abd4064cec9be3eb1bc81d23775680b6ff
Joao-Augusto0 <j.august0rc0@gmail.com>
2023-05-12 10:47:19 -0300

    atualizando criação de equipamento

--- docs/sql/import.sql
+++ docs/sql/import.sql
@@ -1,25 +1,27 @@
-LOAD DATA INFILE "C:/Users/Desenvolvimento/Desktop/Project-TechMan/docs/perfis.csv"
+-- mudar rotas dos arquivos
+
+LOAD DATA INFILE "C:/Users/des/Desktop/Project-TechMan/docs/dados/perfis.csv"
 INTO TABLE perfis
 FIELDS TERMINATED BY ';'
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
 
-LOAD DATA INFILE "C:/Users/Desenvolvimento/Desktop/Project-TechMan/docs/usuarios.csv"
+LOAD DATA INFILE "C:/Users/des/Desktop/Project-TechMan/docs/dados/usuarios.csv"
 INTO TABLE usuarios
 FIELDS TERMINATED BY ';'
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
 
-LOAD DATA INFILE "C:/Users/Desenvolvimento/Desktop/Project-TechMan/docs/equipamentos.csv"
+LOAD DATA INFILE "C:/Users/des/Desktop/Project-TechMan/docs/dados/equipamentos.csv"
 INTO TABLE equipamentos
 FIELDS TERMINATED BY ';'
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
 
-LOAD DATA INFILE "C:/Users/Desenvolvimento/Desktop/Project-TechMan/docs/comentarios.csv"
+LOAD DATA INFILE "C:/Users/des/Desktop/Project-TechMan/docs/dados/comentarios.csv"
 INTO TABLE comentarios
 FIELDS TERMINATED BY ';'
 ENCLOSED BY '"'
